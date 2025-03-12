d1e2dbbbb7a24e6ee1beee238ee96821ad2b2006
shahob-atm <shahobiddinatamurodov@gmail.com>
2025-03-13 01:53:40 +0500

    added docker file

--- src/main/java/com/online/phantom_data/dto/RequestDTO.java
+++ src/main/java/com/online/phantom_data/dto/RequestDTO.java
@@ -1,11 +1,15 @@
 package com.online.phantom_data.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import uz.pdp.app.enums.FieldType;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RequestDTO {
     private String format; // JSON, CSV, SQL
     private List<FieldType> fields; // Tanlangan FieldType lar
