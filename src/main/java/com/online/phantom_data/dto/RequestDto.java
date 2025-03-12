d9bc3f5e4e0124590894fc87fe23337a4d4b5865
shahob-atm <shahobiddinatamurodov@gmail.com>
2025-03-13 02:09:49 +0500

    fixed

--- src/main/java/com/online/phantom_data/dto/RequestDto.java
+++ src/main/java/com/online/phantom_data/dto/RequestDto.java
@@ -1,11 +1,13 @@
 package com.online.phantom_data.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 import uz.pdp.app.enums.FieldType;
 
 import java.util.List;
 
 @Getter
+@Setter
 public class RequestDto {
     private String format; // JSON, CSV, SQL
     private List<FieldType> fields; // Tanlangan FieldType lar
